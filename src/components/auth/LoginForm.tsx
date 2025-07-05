a4f68ce5bc7d0dfd0ce150b097a4d81e5374d865
Felix Avalos <felavalosg@gmail.com>
2025-07-06 06:53:36 +0900

    File structure
    New imports and exports from UI added to the index.ts
    New component to do the register
    Rename login-form.tsx to LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function LoginForm({
+const LoginForm = ({
   className,
   ...props
-}: React.ComponentProps<"form">) {
+}: React.ComponentProps<"form">) => {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
@@ -21,7 +21,7 @@ export function LoginForm({
           <Input id="email" type="email" placeholder="m@example.com" required />
         </div>
         <div className="grid gap-3">
-          <div className="flex items-center">
+          <div className="flex items-center"> { /* Hide and show pwd button */}
             <Label htmlFor="password">Password</Label>
             <a
               href="#"
@@ -35,7 +35,8 @@ export function LoginForm({
         <Button type="submit" className="w-full">
           Login
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+
+        {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
             Or continue with
           </span>
@@ -48,14 +49,17 @@ export function LoginForm({
             />
           </svg>
           Login with GitHub
-        </Button>
+        </Button> */}
+
       </div>
       <div className="text-center text-sm">
         Don&apos;t have an account?{" "}
         <a href="#" className="underline underline-offset-4">
-          Sign up
+          Register
         </a>
       </div>
     </form>
   )
 }
+
+export default LoginForm;
---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+// UI imports
+import { Button } from "./ui/button";
+import { Card } from "./ui/card";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+
+// Auth imports
+import LoginForm from "./auth/LoginForm";
+import RegisterForm from './auth/RegisterForm';
+
+export {
+  // UI exports
+  Button,
+  Card,
+  Label,
+  Input,
+
+  // Auth exports
+  LoginForm,
+  RegisterForm
+}
